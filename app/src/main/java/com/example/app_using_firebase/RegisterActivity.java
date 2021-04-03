fb0b2e1b09678a1c0ae3a97d13d40b594ba086b3
Riddhi73 <80693525+Riddhi73@users.noreply.github.com>
2021-04-03 23:14:18 +0600

    Creating Instagram Clone app

--- app/src/main/java/com/example/app_using_firebase/RegisterActivity.java
+++ app/src/main/java/com/example/app_using_firebase/RegisterActivity.java
@@ -1,67 +1,47 @@
 package com.example.app_using_firebase;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
-
-    private EditText emailEt,passwordEt;
-    private Button registerbtn;
-    private FirebaseAuth firebaseAuth;
+    private EditText username,name,password,email;
+    private DatabaseReference databaseReference;
+    private Button register;
+    private TextView logInUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        emailEt = findViewById(R.id.emailEt);
-        passwordEt = findViewById(R.id.passwordEt);
-        registerbtn = findViewById(R.id.Registerbtn);
-        firebaseAuth = FirebaseAuth.getInstance();
-        registerbtn.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.reg_username);
+        name = findViewById(R.id.reg_fullname);
+        email = findViewById(R.id.reg_email);
+        password = findViewById(R.id.reg_password);
+        register = findViewById(R.id.regBtn);
+        logInUser = findViewById(R.id.login_user);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        logInUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String emailtxt = emailEt.getText().toString();
-                String passwordtxt = passwordEt.getText().toString();
-                if (TextUtils.isEmpty(emailtxt) || TextUtils.isEmpty(passwordtxt)){
-                    Toast.makeText(RegisterActivity.this, "Empty Credential", Toast.LENGTH_SHORT).show();
-                }else if (passwordtxt.length()<6){
-                    Toast.makeText(RegisterActivity.this, "Password Too Short", Toast.LENGTH_SHORT).show();
-                }else{
-                    registerUser(emailtxt,passwordtxt);
-                }
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+
             }
         });
-    }
-
-    private void registerUser(String emailtxt, String passwordtxt) {
-
-
-        firebaseAuth.createUserWithEmailAndPassword(emailtxt,passwordtxt).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull  Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
-                    finish();
-                }else {
-                    Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+                
             }
         });
-
-
     }
 }
--- app/src/main/java/com/example/app_using_firebase/StartActivity.java
+++ app/src/main/java/com/example/app_using_firebase/StartActivity.java
@@ -5,42 +5,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
-    private Button Main_register,Main_login;
 
+    private ImageView icon_Image;
+    private Button logIn,Register;
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Main_register = findViewById(R.id.Main_RegisterBtn);
-        Main_login = findViewById(R.id.Main_LogInBtn);
-        Main_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
-                finish();
-            }
-        });
-        Main_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(StartActivity.this,LogInActivity.class));
-                finish();
-            }
-        });
-    }
+        icon_Image = findViewById(R.id.Picvillage_image);
+        linearLayout = findViewById(R.id.ll1);
+        Register = findViewById(R.id.start_register);
+        logIn = findViewById(R.id.start_login);
+        linearLayout.animate().alpha(0f).setDuration(1);
+        TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+        animation.setDuration(2000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyanimationListener());
+        icon_Image.setAnimation(animation);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (firebaseUser != null){
-            startActivity(new Intent(StartActivity.this,MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class).
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class).
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+    }
+    private class MyanimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            icon_Image.clearAnimation();
+            icon_Image.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
         }
     }
+
 }
