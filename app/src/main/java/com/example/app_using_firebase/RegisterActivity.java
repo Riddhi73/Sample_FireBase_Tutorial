806a4ab0c095f60a9fc15c7472087d0dc004df83
Riddhi73 <80693525+Riddhi73@users.noreply.github.com>
2021-04-04 12:02:14 +0600

    Creating Firebase Cloud Function

--- app/src/main/java/com/example/app_using_firebase/RegisterActivity.java
+++ app/src/main/java/com/example/app_using_firebase/RegisterActivity.java
@@ -1,22 +1,38 @@
 package com.example.app_using_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText username,name,password,email;
     private DatabaseReference databaseReference;
+    private FirebaseAuth auth;
     private Button register;
     private TextView logInUser;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +43,11 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.reg_password);
         register = findViewById(R.id.regBtn);
         logInUser = findViewById(R.id.login_user);
+        progressDialog = new ProgressDialog(this);
 
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
         logInUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,8 +58,59 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                String txtUsername = username.getText().toString();
+                String txtName = name.getText().toString();
+                String txtEmail = email.getText().toString();
+                String txtPassword = password.getText().toString();
+                if (TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtName) || TextUtils.isEmpty(txtPassword)){
+                    Toast.makeText(RegisterActivity.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+                }else if (txtPassword.length()<6){
+                    Toast.makeText(RegisterActivity.this, "Password Too Short", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    registerUser(txtUsername,txtName,txtEmail,txtPassword);
+                }
+
             }
         });
+    }
+
+    private void registerUser(String txtUsername, String txtName, String txtEmail, String txtPassword) {
+        progressDialog.setMessage("Please Wait");
+        progressDialog.show();
+
+        auth.createUserWithEmailAndPassword(txtEmail,txtPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                HashMap<String,Object> map = new HashMap<>();
+                map.put("Name",txtName);
+                map.put("Email",txtEmail);
+                map.put("Username",txtUsername);
+                map.put("id",auth.getCurrentUser().getUid());
+                databaseReference.child("Users").child(auth.getCurrentUser().getUid()).setValue(map)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    progressDialog.dismiss();
+                                    Toast.makeText(RegisterActivity.this, "Update the profile" +
+                                            "for better experience", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this,MainActivity.class)
+                                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
